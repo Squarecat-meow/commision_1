37ce22e755c8f35f8366a697e4d381e556f77ed5
Squarecat-meow <yozumina@serafuku.moe>
2026-02-06 19:23:04 +0900

    Tanstack Query 개발자 도구 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const client = new QueryClient();
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient;
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = client;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
